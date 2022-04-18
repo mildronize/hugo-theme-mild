223be38426f32364bbdf3c71de863b24a871cbf4
Thada Wangthammang <thada.wth@gmail.com>
2022-04-18 21:26:02 +0700

    setup webpack dev path for local dev theme

--- .config/prepare-hugo-config.js
+++ .config/prepare-hugo-config.js
@@ -3,8 +3,12 @@ const fs = require("fs");
 
 const source = process.argv[2];
 const destination = process.argv[3];
+const webpackOutputPath = process.argv[4];
 
 const file = fs.readFileSync(source, "utf8");
 const data = yaml.parse(file);
-data.baseURL = "/";
+// Use root path for local dev instead
+data.baseURL = '/';
+// Setup prefix path for webpack output for local dev
+data.params.webpackOutputPath = webpackOutputPath || "assets";
 fs.writeFileSync(destination, yaml.stringify(data), "utf8");
